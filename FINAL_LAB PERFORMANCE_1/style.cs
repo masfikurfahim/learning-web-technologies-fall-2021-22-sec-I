3b6c75c6adcd725365fdf13f769328e34528a9e2
Masfikur Fahim <masfikur5@gmail.com>
2021-11-21 15:53:39 +0600

    Second Commit

---
+++ FINAL_LAB PERFORMANCE_1/style.cs
@@ -0,0 +1,13 @@
+.container {
+    max-width: 400px;
+    margin: 10vh auto 0 auto;
+    box-shadow: 0px 0px 43px 17px rgba(153,153,153,1);
+}
+
+#display {
+    text-align: right;
+    height: 60px;
+    line-height: 60px;
+    padding: 16px 8px;
+    font-size: 25px;
+}
